c0b34e29474e78bdf6878aeb423b7cfe945a7e3c
pry5th0n <rennandelcastillo@gmail.com>
2019-11-22 19:27:05 -0300

    Initial commit

---
+++ src/main/java/br/desafio/sefaz/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.desafio.sefaz;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SefazApplication.class);
+	}
+
+}
